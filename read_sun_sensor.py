ba409c76a5c12355fd7ec2ca1bd550068c05496e
Hugo Hadfield <hadfield.hugo@gmail.com>
2023-08-26 01:11:38 +0100

    Make good progress

---
+++ read_sun_sensor.py
@@ -0,0 +1,28 @@
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def with_sun_as_unit_vector_in_flu(data: pd.DataFrame):
+    sin_zen = np.sin(np.deg2rad(data['zen']))
+    cos_zen = np.cos(np.deg2rad(data['zen']))
+    cos_az = np.cos(np.deg2rad(data['az']))
+    sin_az = np.sin(np.deg2rad(data['az']))
+    data['sun_f'] = -sin_zen*sin_az
+    data['sun_l'] = sin_zen*cos_az
+    data['sun_u'] = cos_zen
+    data['time'] = pd.to_datetime(data['time'])
+
+
+if __name__ == '__main__':
+    data = pd.read_csv('example_dataset/raw_data/sun-sensor.txt', sep="\s+|\t+|\s+\t+|\t+\s+", header=None, names=['time', 'az', 'zen'])
+    with_sun_as_unit_vector_in_flu(data)
+    print(data)
+
+    # plt.plot(data['sun_f'])
+    # plt.plot(data['sun_l'])
+    # plt.plot(data['sun_u'])
+    # plt.hist(data['az'], bins=100)
+    plt.plot(data['time'], data['az'], '-')
+    plt.show()
